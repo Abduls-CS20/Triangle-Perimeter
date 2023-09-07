4eae80aa1b41b4c9f172dd268e59ecc26b85c0c7
Deadabdul <a.oguntade11@gmail.com>
2023-09-07 14:52:27 -0600

    umm

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 // Triangle Perimeter Assignment Start Code
+document.getElementById("btn").addEventListener("click", btnClicked)
+
+
+function btnClicked()
+{
+    
+}
+function calculateHypotenuse(a, b) {
+
+    // Calculate the square of 'a' and 'b'
+    const squareA = a * a;
+    const squareB = b * b;
+
+    // Calculate the square of the hypotenuse
+    const squareHypotenuse = squareA + squareB;
+
+    // Calculate the length of the hypotenuse by taking the square root
+    return Math.sqrt(squareHypotenuse);
+}
